c11cf8639696f5467d2ac1edd6b92b59d7a6744b
meghanad-kayanattil <71318851+meghanad-kayanattil@users.noreply.github.com>
2022-07-20 16:09:34 +0200

    Location information is incluced

--- Rent prediction wt linear regression-Hamburg-2022-July/web_scraper.py
+++ Rent prediction wt linear regression-Hamburg-2022-July/web_scraper.py
@@ -7,46 +7,53 @@ from bs4 import BeautifulSoup
 import requests
 
 i = 1
-
 price_all = []
 area_all = []
-room_all = [] 
+room_all = []
+districts = []
+
 while i != 45:
-      url = f'https://www.immowelt.de/liste/hamburg/wohnungen/mieten?sort=relevanz&sp={i}'
-      print(url)
-      i = i + 1
-      
+    url = f'https://www.immowelt.de/liste/hamburg/wohnungen/mieten?sort=relevanz&sp={i}'
+    i = i + 1
+    page = requests.get(url)
+    print(page)
+    # Getting the data from the website
+    soup = BeautifulSoup(page.content, 'html.parser')
+    # print(soup())
+    # Finding all data belonging to the class keyfacts*
+    # lists = soup.find_all('div', class_='KeyFacts-efbce')
+    price = soup.find_all('div', attrs={"data-test": "price"})
+    area = soup.find_all('div', attrs={'data-test': 'area'})
+    rooms = soup.find_all('div', attrs={'data-test': 'rooms'})
+    for p, a, r in zip(price, area, rooms):
+        print(f'{p.text} {a.text} {r.text}')
+        p_end = ' €'
+        r_end = ' Zi.'
+        a_end = ' m²'
+        price_all.append(p.text[:-len(p_end)])
+        area_all.append(a.text[:-len(a_end)])
+        room_all.append(r.text[:-len(r_end)])
+    list_location = soup.find_all('div', class_='IconFact-e8a23')
+    j = 0
+    for loc in list_location:
+        strip = 'location'
+        location_tmp = loc.text
+        if strip in location_tmp:
+            location = location_tmp[len(strip):]
+            before, sep, after = location.partition('Hamburg')
+            start = '('
+            end = ')'
+            district = after[after.find(start)+len(start):after.rfind(end)]
+            print(f'{j} = {district}')
+            j += 1
+            districts.append(district)
+    # location = soup.find_all('div')
+# %% Writing the scraped data into file
+file = open('housing_data_hamburg_v3.txt', 'w+')
 
-      page = requests.get(url)
-      print(page)
-
-      soup = BeautifulSoup(page.content, 'html.parser')
-        #print(soup())
-
-      
-      lists = soup.find_all('div', class_='KeyFacts-efbce')
-
-      price = soup.find_all('div', attrs={"data-test":"price"})
-      area = soup.find_all('div', attrs = {'data-test':'area'})
-      rooms = soup.find_all('div', attrs = {'data-test':'rooms'}) 
-      
-
-      for p, a, r in zip(price, area, rooms):
-          print(f'{p.text} {a.text} {r.text}')
-          
-          price_all.append(p.text)
-          area_all.append(a.text)
-          room_all.append(r.text)
-          
-file = open('housing_data_hamburg_v2.txt', 'w+')
-
-head = 'Area'+'\t'+'Rooms'+'\t'+'Rent'+'\n'
+head = 'Area'+'\t'+'Rooms'+'\t'+'Location'+'\t'+'Rent'+'\n'
 
 file.write(head)
-for p, a, r in zip(price_all, area_all, room_all):
-    file.write(str(a)+'\t'+str(r)+'\t'+str(p)+'\n')
+for p, a, d, r in zip(price_all, area_all, districts, room_all):
+    file.write(a+'\t'+r+'\t'+d+'\t'+p+'\n')
 file.close()
-
-
-
-    
